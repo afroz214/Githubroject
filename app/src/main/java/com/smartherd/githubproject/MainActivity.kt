19c0eda0cb6f0bd2743009b082bb20adfff55630
Your Name <you@example.com>
2020-02-02 02:36:03 +0530

    updated a button

--- app/src/main/java/com/smartherd/githubproject/MainActivity.kt
+++ app/src/main/java/com/smartherd/githubproject/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.smartherd.githubproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //my first github project
+        //my first mistake on github
+        btn_next.setOnClickListener {
+            startActivity(Intent(this@MainActivity, secondactivtiy::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/com/smartherd/githubproject/secondactivtiy.kt
@@ -0,0 +1,12 @@
+package com.smartherd.githubproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secondactivtiy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondactivtiy)
+    }
+}
